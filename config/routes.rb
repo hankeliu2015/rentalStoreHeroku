871bc94b1d2c54a52781d544cfb8f877a2eeb07d
ghankeliu2015 <hanke.liu@gmail.com>
2019-03-26 00:19:55 -0400

    add tools controller and index and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tools/index'
+  get 'tools/show'
   root 'tools#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
