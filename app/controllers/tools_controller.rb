e72366b79a03b11f4f6627c95c3669854a1710be
ghankeliu2015 <hanke.liu@gmail.com>
2019-03-29 12:27:50 -0400

    add controller action set_tool for tools controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,6 @@
 class ToolsController < ApplicationController
+before_action :set_tool, only: [:show, :edit, :update, :destroy]
+
   def index
     #binding.pry
     @tools = Tool.all
@@ -6,6 +8,13 @@ class ToolsController < ApplicationController
 
   def show
     #binding.pry
+    #@tool = Tool.find_by(id: params[:id])
+  end
+
+  private
+
+  def set_tool
     @tool = Tool.find_by(id: params[:id])
   end
-end
+
+end #end of class.
