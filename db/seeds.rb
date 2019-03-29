f5a568e493482e24c443e690c0622e4321fce1ab
ghankeliu2015 <hanke.liu@gmail.com>
2019-03-29 17:59:37 -0400

    add some seed file for tools table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Tool.create(name: "Floor Polisher", category: "Floor Buffers and Sanders", brand: "Clarke American", description: "For indoor floor only", condition: "fair", availability: true)
-# Tool.create(name: "Wet Vacuum", category: "Vaccums" , brand: "Karcher", description: "Only for water under room temperature", condition: "good", availability: true)
 
 #  Rental.create(start_date: 01/01/2020, return_date: 02/01/2020, return: false, user_id: 1, tool_id: 1)
 # # #
 #  Rental.create(start_date: 2020_01_01, return_date: 2020_02_01, return: false, user_id: 2, tool_id: 2)
+
+# Tool.create(name: "Floor Polisher", category: "Floor Buffers and Sanders", brand: "Clarke American", description: "For indoor floor only", condition: "fair", availability: true)
+# Tool.create(name: "Wet Vacuum", category: "Vaccums" , brand: "Karcher", description: "Only for water under room temperature", condition: "good", availability: true)
+
+Tool.create(name: "Drum Floor Sander", category: "Floor Buffers and Sanders", brand: "Clarke American", description: "ideal for refinishing old or distressed flooring", condition: "good", availability: true)
+Tool.create(name: "Dry Wall Dust Vacuum", category: "Vaccums" , brand: "Makita", description: "Pair with many dust generating tools to effectively collect airborne particles", condition: "good", availability: true)
