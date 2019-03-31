b7ea8d6fb4b97ebd1a89139bf476c0097a72addc
ghankeliu2015 <hanke.liu@gmail.com>
2019-03-30 22:45:00 -0400

    add past_rent_tool list on user's show page. Add instance method on Tool class and class method on Rental class.

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,12 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :tool
-end
+
+  def self.past_tool_rental(tool, user)
+
+    @past_rental = where(tool_id: tool.id, user_id: user.id).where(return: true)
+
+    @past_rental[0].return_date if @past_rental != [] #???
+  end
+
+end #end of class
