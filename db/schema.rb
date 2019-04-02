ed4cb99ba64f82f6166970163afa90e7f762c665
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-01 22:35:56 -0400

    add username attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_214113) do
+ActiveRecord::Schema.define(version: 2019_04_02_022243) do
 
   create_table "rentals", force: :cascade do |t|
     t.datetime "start_date"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_214113) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
