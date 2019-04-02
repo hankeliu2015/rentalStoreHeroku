34b010fb750ff75a79cce841dd2666d04e3181f4
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-02 16:08:56 -0400

    add helpers method in RentalsHelper module, display_return_date, display_start_date, display_actual_return_date.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,6 +1,7 @@
 class RentalsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+
   def new
     @rental = Rental.new(tool_id: params[:tool_id])
   end
--- app/helpers/rentals_helper.rb
+++ app/helpers/rentals_helper.rb
@@ -1,2 +1,19 @@
 module RentalsHelper
-end
+
+  def display_return_date(rental)
+    rental.return_date.strftime("%a, %b %e, %y")
+  end
+
+  def display_start_date(rental)
+    rental.start_date.strftime("%a, %b %e, %y")
+  end
+
+  def display_actual_return_date(rental) # not dry
+    rental.actual_return_date.strftime("%a, %b %e, %y") if rental.actual_return_date
+  end
+
+  # def return_ontime?(rental) # not working?
+  #   rental.return_date <= DateTime.new && rental.return == true
+  # end
+
+end #end of class
