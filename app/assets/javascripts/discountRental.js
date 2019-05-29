ff037b200ed7abf852210559ad491874d63c4c62
github username <github email address>
2019-05-29 19:09:34 -0400

    update dicountRental.js to render a partial with params.

--- app/assets/javascripts/discountRental.js
+++ app/assets/javascripts/discountRental.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-  $('a#discount-rental').on('click', displayRentalForm
+  $('a#discount-rental').on('click', displayRentalForm)
 })
 
 function displayRentalForm(e) {
   e.preventDefault();
+  let discoutToolId = parseInt(e.target.dataset.tool_id)
   console.log("test rental form")
+
+  $('.hidden').html(`<%= j render partial: 'rentals/form', locals: {user_rental: Rental.new(start_date: Date.today, return_date: Date.tomorrow, tool_id:${discoutToolId})} %>`);
 }
