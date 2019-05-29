89812e44ac6ce248a2829b7b0db10ea12a5abc06
github username <github email address>
2019-05-29 17:56:55 -0400

    add discountRental.js to asset

---
+++ app/assets/javascripts/discountRental.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('a#discount-rental').on('click', displayRentalForm
+})
+
+function displayRentalForm(e) {
+  e.preventDefault();
+  console.log("test rental form")
+}
