8d8fa032f22cb88b4218c00e28f77b0cad97fe88
github username <github email address>
2019-05-28 21:38:46 -0400

    corrected forEach syntax. Add Date prototype toDateString method convert start date and return date to correct format.

--- app/assets/javascripts/rentalsHistory.js
+++ app/assets/javascripts/rentalsHistory.js
@@ -1,7 +1,25 @@
-$(document).ready(function() {
-  console.log("jQuery test")
-  $(".rentals-history").on("click", function(){
-    console.log("link clicked")
-  })
 
+
+$(document).ready(function() {
+
+  $(".rentals-history").on("click", function(e){
+    e.preventDefault();
+    $.get("/rentals" + ".json", function(data) {
+
+      data.forEach((el, i) => {
+        let custom_start_date = new Date(el.start_date)
+        let custom_return_date = new Date(el.return_date)
+
+        let val = `
+          <tr>
+            <td>${i+1}</td>
+            <td>${el.name}</td>
+            <td>${custom_start_date.toDateString()}</td>
+            <td>${custom_return_date.toDateString()}</td>
+          </tr>
+        `
+        $(".list-rentals-history").append(val);
+      })
+    })
+  })
 })
