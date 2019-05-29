1a3eb38bb5b01754c7fcc3c08cc0ac06e9ddba67
github username <github email address>
2019-05-29 19:07:16 -0400

    add static routes for rentals#create

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
+  post 'rentals', to: 'rentals#create'
   get 'rentals/new'
   get 'profile', to: 'users#show'
   get 'rentals', to: 'rentals#index'
