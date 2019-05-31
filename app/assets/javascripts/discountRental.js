f766c8d4547080764fcda7c0830cfda602f1dc88
github username <github email address>
2019-05-31 10:10:03 -0400

    change form element class formDiscountRent to element id.

--- app/assets/javascripts/discountRental.js
+++ app/assets/javascripts/discountRental.js
@@ -7,23 +7,25 @@ function displayRentalForm(e) {
   let discoutToolId = parseInt(e.target.dataset.toolId) //tool_id not working.
 
   const discountRentForm = $(".hidden").html()
-  $(`.formDiscountRental-${discoutToolId}`).html(discountRentForm)
+  $(`#formDiscountRental-${discoutToolId}`).html(discountRentForm)
   $(".hidden").html("")
 
   // $.('form')?? && toolId??
-  $(`.formDiscountRental-${discoutToolId}`).submit(function(event) {
+  $(`#formDiscountRental-${discoutToolId}`).submit(function(event) {
+    e.preventDefault();
     debugger
     createDiscountRentalObj(event, toolId)
   })
 
-  // do not delete yet. 
+  // do not delete yet.
   // $('.hidden').html(`<%= j render partial: 'rentals/form', locals: {user_rental: Rental.new(start_date: Date.today, return_date: Date.tomorrow, tool_id:${discoutToolId})} %>`);
 }
 
 function createDiscountRentalObj(e, idTool) {
   e.preventDefault();
-  let value = $(`.formDiscountRental-${discoutToolId}`).Serializer()
-
+  let value = $(this).serialize();
+  let renting = $.post('rentals', value)
+debugger
   //??
 
   // $.post('')
