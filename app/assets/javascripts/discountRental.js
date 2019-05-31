2ab9ab2938ffdf131b37a827b39663797e602209
github username <github email address>
2019-05-30 23:24:06 -0400

    update displayrentalForm function to render new discount rental form and eventhandler to submit the form. Add createDiscountRentalObj function.

--- app/assets/javascripts/discountRental.js
+++ app/assets/javascripts/discountRental.js
@@ -4,9 +4,27 @@ $(document).ready(function(){
 
 function displayRentalForm(e) {
   e.preventDefault();
-  let discoutToolId = parseInt(e.target.dataset.toolId) //tool_id not working. 
-  console.log(`test display rental form - test tool ID : ${discoutToolId}`)
+  let discoutToolId = parseInt(e.target.dataset.toolId) //tool_id not working.
 
-debugger
+  const discountRentForm = $(".hidden").html()
+  $(`.formDiscountRental-${discoutToolId}`).html(discountRentForm)
+  $(".hidden").html("")
+
+  // $.('form')?? && toolId??
+  $(`.formDiscountRental-${discoutToolId}`).submit(function(event) {
+    debugger
+    createDiscountRentalObj(event, toolId)
+  })
+
+  // do not delete yet. 
   // $('.hidden').html(`<%= j render partial: 'rentals/form', locals: {user_rental: Rental.new(start_date: Date.today, return_date: Date.tomorrow, tool_id:${discoutToolId})} %>`);
+}
+
+function createDiscountRentalObj(e, idTool) {
+  e.preventDefault();
+  let value = $(`.formDiscountRental-${discoutToolId}`).Serializer()
+
+  //??
+
+  // $.post('')
 }
