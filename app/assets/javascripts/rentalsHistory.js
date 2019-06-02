77f6ca8d0386dbbbee2d5d82febe4b3a725b0422
github username <github email address>
2019-06-02 12:21:19 -0400

    add data attribute data-toolId into rental prototype method tool_name anchor element. Attach eventhandler function for click on .show-tool class.

--- app/assets/javascripts/rentalsHistory.js
+++ app/assets/javascripts/rentalsHistory.js
@@ -19,6 +19,13 @@ const rentalsHistoryClickHandlers = function() {
       })
     })
   })
+
+  $(document).on("click", ".show-tool", function(e) {
+    e.preventDefault();
+    debugger
+    console.log(this)
+    fetch("/rentals.json")
+  })
 }
 
 function Rental(rental) {
@@ -36,7 +43,7 @@ Rental.prototype.formatRentalsHistory = function() {
   let val = `
   <tr>
     <td>${this.id}</td>
-    <td><a href="/tools/${this.tool_id}" class="show-tool">${this.tool_name}</a></td>
+    <td><a href="/tools/${this.tool_id}" data-toolId="${this.tool_id}" class="show-tool">${this.tool_name}</a></td>
     <td>${custom_start_date.toDateString()}</td>
     <td>${custom_return_date.toDateString()}</td>
   </tr>
