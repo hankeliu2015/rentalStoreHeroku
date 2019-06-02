54d78e5db94ef9e5b4a1d2a85b93e9d53b421a5a
github username <github email address>
2019-06-01 20:46:54 -0400

    rewrite rentalHistory.js with JS constructor syntax. create rentalsHistoryClickHandlers.

--- app/assets/javascripts/rentalsHistory.js
+++ app/assets/javascripts/rentalsHistory.js
@@ -1,25 +1,35 @@
-
-
 $(document).ready(function() {
+  rentalHistoryClickHandlers()
+})
 
+const rentalHistoryClickHandlers = function() {
   $(".rentals-history").on("click", function(e){
     e.preventDefault();
-    $.get("/rentals" + ".json", function(data) {
-
-      data.forEach((el, i) => {
-        let custom_start_date = new Date(el.start_date)
-        let custom_return_date = new Date(el.return_date)
-        // debugger
-        let val = `
-          <tr>
-            <td>${i+1}</td>
-            <td><a href=#>${el.tool.name}</a></td>
-            <td>${custom_start_date.toDateString()}</td>
-            <td>${custom_return_date.toDateString()}</td>
-          </tr>
-        `
-        $(".list-rentals-history").append(val);
-      })
-    })
+    console.log("history rentals clicked")
   })
-})
+}
+
+
+// $(document).ready(function() {
+//
+//   $(".rentals-history").on("click", function(e){
+//     e.preventDefault();
+//     $.get("/rentals" + ".json", function(data) {
+//
+//       data.forEach((el, i) => {
+//         let custom_start_date = new Date(el.start_date)
+//         let custom_return_date = new Date(el.return_date)
+//         // debugger
+//         let val = `
+//           <tr>
+//             <td>${i+1}</td>
+//             <td><a href=#>${el.tool.name}</a></td>
+//             <td>${custom_start_date.toDateString()}</td>
+//             <td>${custom_return_date.toDateString()}</td>
+//           </tr>
+//         `
+//         $(".list-rentals-history").append(val);
+//       })
+//     })
+//   })
+// })
