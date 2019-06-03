53e98f384054af83e48acbec5354989a639394b8
github username <github email address>
2019-06-02 20:33:35 -0400

    add Tool function and Tool prototype format method. Render the tool attributes without leaving homepage.

--- app/assets/javascripts/showTool.js
+++ app/assets/javascripts/showTool.js
@@ -8,11 +8,38 @@ const showToolClickHandlers = function(){
     let id = parseInt(this.dataset["toolId"])
     fetch(`tools/${id}.json`)
     .then(function(res) {
-      debugger
       return res.json()
     })
     .then(function(tool){
       console.log(tool)
+      $("#tool-container").html("")
+      
+      let newTool = new Tool(tool)
+      let toolHTML = newTool.formatTool()
+      $("#tool-container").html(toolHTML)
     })
   })
+}
+
+function Tool (tool) {
+  this.id = tool.id
+  this.name = tool.name
+  this.brand = tool.Brand
+  this.description = tool.description
+  this.condition = tool.condition
+  this.rental_price = tool.rental_price
+  this.image = tool.image
+}
+
+Tool.prototype.formatTool = function() {
+  let val = `
+    <h4> ${this.name} </h4>
+    <ul>
+      <li>${this.description} </li>
+      <li>${this.brand} </li>
+      <li>${this.rental_price} </li>
+
+    </ul>
+  `
+  return val
 }
