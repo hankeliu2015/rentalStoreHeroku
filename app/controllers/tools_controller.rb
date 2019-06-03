d4a5e969f5546304bcfe3da77d4b85916fad8f0f
github username <github email address>
2019-06-03 18:59:27 -0400

    not syntax change. format update

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -9,9 +9,11 @@ before_action :set_tool, only: [:show, :edit, :update, :destroy]
   def show
     #binding.pry
     #@tool = Tool.find_by(id: params[:id])
+    @in_progress = @tool.rentals.in_progress
     respond_to do |f|
       f.html {render :show}
       f.json {render json: @tool}
+      # f.json {render json: @in_progress}
     end
   end
 
