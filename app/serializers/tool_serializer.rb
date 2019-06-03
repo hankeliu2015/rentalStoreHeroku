6c6a212383c07f2c8d428a3b6d1b16aa3ba54c09
github username <github email address>
2019-06-03 18:56:42 -0400

    add rental_in_progress

--- app/serializers/tool_serializer.rb
+++ app/serializers/tool_serializer.rb
@@ -1,4 +1,5 @@
 class ToolSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :description, :condition, :rental_price, :image
+  attributes :id, :name, :brand, :description, :condition, :rental_price, :image, :rental_in_progress
   has_many :rentals
+
 end
