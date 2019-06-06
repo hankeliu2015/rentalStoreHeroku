b39a23dd162b6da7c79d2cfe630e1694da71951c
github username <github email address>
2019-06-06 13:08:09 -0400

    add the customized serializer relationship to rental serializer.

--- app/serializers/rental_serializer.rb
+++ app/serializers/rental_serializer.rb
@@ -1,5 +1,5 @@
 class RentalSerializer < ActiveModel::Serializer
-  attributes :id, :tool_id, :start_date, :return_date #:in_progress
+  attributes :id, :tool_id, :start_date, :return_date
   belongs_to :tool
-  belongs_to :user
+  belongs_to :user, serializer: UserReturnedRentalsSerializer
 end
