61cf8480098d827acc955b1a10faeea0353398f7
github username <github email address>
2019-06-06 16:03:08 -0400

    replace returned_retals with returned_rentals_with_tool_name method.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :returned_rentals
-  has_many :rentals
-  has_many :tools, through: :rentals
+  attributes :id, :username, :email, :returned_rentals_with_tool_name #, :returned_rentals
+  # has_many :rentals
+  # has_many :tools, through: :rentals
 end
