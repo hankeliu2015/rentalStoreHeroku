a5040f683979d976a8786c318f85922f6b8cb8fd
github username <github email address>
2019-06-06 10:20:32 -0400

    add @user for json responder.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,26 +4,28 @@ class UsersController < ApplicationController
 
 
   def show
-    # use current_user to replace user id in URL. repalce all @user with current_user
 
       @in_prossession = current_user.rentals.in_progress
-      # binding.pry
+
       @scheduled_rentals = current_user.rentals.scheduled
 
       @forget_checkout = current_user.rentals.forget_checkout
 
       @overdue_items = current_user.rentals.overdued
-      # the following code no needed. User show only only show a link for completed.
-      # @past_rented_tools = current_user.rentals.completed
-      # @user = User.find_by(id: params[:id])
 
-      # respond_to do |f|
-      #   f.html {render :show}
-      #   f.json {render json: @user }
-      # end
+      @user = current_user #use @user to render serializer. 
+
+      respond_to do |f|
+        f.html {render :show}
+        f.json {render json: @user }
+      end
+
+      # render json: @user.to_json
       # render json: @user
-      
+
   end
+
+  # use current_user to replace user id in URL. repalce all @user with current_user
 
   private
   def set_user
