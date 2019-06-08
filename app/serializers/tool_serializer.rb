579718b53ac0238bb3652da706108b2a1483c018
github username <github email address>
2019-06-07 21:20:55 -0400

    add rentals_scheduled attribute to tool serializer.

--- app/serializers/tool_serializer.rb
+++ app/serializers/tool_serializer.rb
@@ -1,5 +1,5 @@
 class ToolSerializer < ActiveModel::Serializer
-  attributes :id, :name, :brand, :description, :condition, :rental_price, :image, :rental_in_progress, :rental_overdued
+  attributes :id, :name, :brand, :description, :condition, :rental_price, :image, :rental_in_progress, :rental_overdued, :rentals_scheduled
   has_many :rentals
 
 end
