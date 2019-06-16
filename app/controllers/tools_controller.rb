b03a61efb4ef2ab733f784971fca05778c1ac340
github username <github email address>
2019-06-15 20:11:30 -0400

    clean up comments, no syntax change

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,18 +2,14 @@ class ToolsController < ApplicationController
 before_action :set_tool, only: [:show, :edit, :update, :destroy]
 
   def index
-    #binding.pry
     @tools = Tool.all
   end
 
   def show
-    #binding.pry
-    #@tool = Tool.find_by(id: params[:id])
     @in_progress = @tool.rentals.in_progress
     respond_to do |f|
       f.html {render :show}
       f.json {render json: @tool}
-      # f.json {render json: @in_progress}
     end
   end
 
@@ -23,4 +19,4 @@ before_action :set_tool, only: [:show, :edit, :update, :destroy]
     @tool = Tool.find_by(id: params[:id])
   end
 
-end #end of class.
+end
