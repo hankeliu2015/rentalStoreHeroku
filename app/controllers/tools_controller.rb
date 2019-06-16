9fb0924b1ca40f1f8ff13f4b4fb523b9f78484a7
github username <github email address>
2019-06-15 21:31:29 -0400

    add conditions to display overdued and rental_in_progress tool message and start return date

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,6 +6,8 @@ before_action :set_tool, only: [:show, :edit, :update, :destroy]
   end
 
   def show
+    @rental_in_progress_message = "This tool is not available right now. Please scheulde rentals date base on tool return date:"
+    @rental_overdued_message = "This tool is overdued, not available for rent. Sorry!"
     @in_progress = @tool.rentals.in_progress
     respond_to do |f|
       f.html {render :show}
