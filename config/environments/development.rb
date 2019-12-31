6a955838cbba6c241fa90cbb97bf2843fa60fc04
github username <github email address>
2019-12-30 23:43:58 -0500

    generate notify mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,13 +37,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   #SMTP settings for Mailgun
-  ActionMailer:::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     port: 587,
     address: "smtp.mailgun.org",
     domain: "my domain",
     user_name: ENV["MAILGUN_USERNAME"],
     password: ENV["MAILGUN_PASSWORD"],
-    authentication: plain,
+    authentication: 'plain',
   }
 
   # Print deprecation notices to the Rails logger.
