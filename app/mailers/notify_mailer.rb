cc2c3fcf04c9abea40b43291da41e96a744306b5
github username <github email address>
2019-12-30 23:48:59 -0500

    add notification method for notifymail class

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,2 +1,11 @@
 class NotifyMailer < ApplicationMailer
+  def notification(user)
+    @user = user
+    mail(
+      from: @user.email,
+      to:"hanke.liu@gmail.com",
+      content_type: "text/plain",
+      subject: "Welcome #{{@user.name}} Register at Rental Store"
+    )
+  end
 end
