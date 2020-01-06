8684454d67375ca2a609fd3f680a4bae4fc81542
github username <github email address>
2020-01-06 15:28:47 -0500

    add intializer for mailgn gem and correct mailgun login and password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,33 +31,35 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  #SMTP settings for Mailgun
-  # ActionMailer::Base.smtp_settings = {
-  #   port: 587,
-  #   address: "smtp.mailgun.org",
-  #   domain: ENV["MAILGUN_DOMAIN"],
-  #   user_name: ENV["MAILGUN_USERNAME"],
-  #   password: ENV["MAILGUN_PASSWORD"],
-  #   authentication: 'plain',
-  # }
+
+  ## SMTP settings for Mailgun
+
+  ActionMailer::Base.smtp_settings = {
+    port: 587,
+    address: "smtp.mailgun.org",
+    domain: ENV["MAILGUN_DOMAIN"],
+    user_name: ENV["MAILGUN_USERNAME"],
+    password: ENV["MAILGUN_PASSWORD"],
+    authentication: 'plain',
+  }
 
   # SMTP setting from storeappnotifier@gmail account:
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: ENV["EMAIL_USER_NAME"],
-    password: ENV["EMAIL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-   }
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "gmail.com",
+  #   user_name: ENV["EMAIL_USER_NAME"],
+  #   password: ENV["EMAIL_PASSWORD"],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  #  }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
